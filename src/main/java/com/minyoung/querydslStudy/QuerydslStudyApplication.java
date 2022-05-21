2ba35611b7ed8bc4cb00ff83a2501d778a4810e4
minyoung62 <tnd05106@naver.com>
2022-05-21 15:57:30 +0900

    feat: querydsl study setting

---
+++ src/main/java/com/minyoung/querydslStudy/QuerydslStudyApplication.java
@@ -0,0 +1,13 @@
+package com.minyoung.querydslStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslStudyApplication.class, args);
+	}
+
+}
